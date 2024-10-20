d9a215fd1690498376c23788ea9e6b67012d5e78
Jacob Sanford <jsanford@unb.ca>
2024-10-20 10:14:08 -0300

    Update flask routes for file

--- deckforge/commands/cardadmin.py
+++ deckforge/commands/cardadmin.py
@@ -1,9 +1,12 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, send_from_directory
 import json
 import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 DATA_FILE = '/home/jsanford/gitDev/deckforge/data/card_metadata.json'
+UPLOAD_FOLDER = '/home/jsanford/gitDev/deckforge/static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def read_data():
     if os.path.exists(DATA_FILE):
@@ -27,12 +30,16 @@ def get_cards():
 
 @app.route('/cards', methods=['POST'])
 def add_card():
-    new_card = request.json
+    new_card = request.form.to_dict()
+    image_file = request.files.get('cardImage')
+    if image_file:
+        filename = secure_filename(image_file.filename)
+        image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        image_file.save(image_path)
+        new_card['image_path'] = f'/static/uploads/{filename}'
+
     data = read_data()
-    new_card_id = 0
-    for card in data:
-        if card['id'] > new_card_id:
-            new_card_id = card['id'] + 1
+    new_card_id = max([card['id'] for card in data], default=0) + 1
     new_card['id'] = new_card_id
     data.append(new_card)
     write_data(data)
@@ -46,10 +53,16 @@ def get_card(card_id):
             return jsonify(card)
     return jsonify({'error': 'Card not found'}), 404
 
-
 @app.route('/cards/<int:card_id>', methods=['PUT'])
 def update_card(card_id):
-    updated_card = request.json
+    updated_card = request.form.to_dict()
+    image_file = request.files.get('cardImage')
+    if image_file:
+        filename = secure_filename(image_file.filename)
+        image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        image_file.save(image_path)
+        updated_card['image_path'] = f'/static/uploads/{filename}'
+
     data = read_data()
     for card in data:
         if card['id'] == card_id:
@@ -61,16 +74,17 @@ def update_card(card_id):
 @app.route('/cards/<int:card_id>', methods=['DELETE'])
 def delete_card(card_id):
     data = read_data()
-    card_to_delete = {}
-    for card in data:
-        if card['id'] == card_id:
-            card_to_delete = card
-    if card_to_delete == {}:
+    card_to_delete = next((card for card in data if card['id'] == card_id), None)
+    if not card_to_delete:
         return jsonify({'error': 'Card not found'}), 404
 
     data.remove(card_to_delete)
     write_data(data)
     return jsonify(card_to_delete)
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 def start_admin_server():
     app.run(debug=True)
